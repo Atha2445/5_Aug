e0a7de0ce516cd596df7a065e0d5231ef6e90f89
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-08-05 20:14:09 +0530

    Add files via upload

---
+++ &&.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("e t v=");
+    scanf("%d%d",&a,&b);
+    c=a>b?a:b;
+    printf("%d is greater",c);
+    return 0;
+
+}
+/*
+lenovo idea pad slim 5i
+11th gen intel gen 5
+with offer 61k without offer 70-73k */
+
+
---
+++ code10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a = (4,5);
+    b=4,5;
+    printf("a=%d,b=%d",a,b);
+
+    return 0;
+}
